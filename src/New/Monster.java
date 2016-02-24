237a62d55b1a6863b43848d937225a7518890775
ElliottP-13 <pryorelliott@gmail.com>
2016-02-23 18:59:20 -0700

    Bad practice "Read Only" fix
    Changed pickMonster() to static so it can be referenced from other
    classes. Also changed ArrayList AllMonsters to static so it can be
    referenced from pickMonster(), shouldn't cause problems though...

--- src/New/Monster.java
+++ src/New/Monster.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 import java.util.Random;
 
 public class Monster {
-	ArrayList<Monster> AllMonsters = new ArrayList<Monster>(10);
+	static ArrayList<Monster> AllMonsters = new ArrayList<Monster>(10);
 	String name;
 	int xp;
 	int gold;
@@ -13,24 +13,26 @@ public class Monster {
 	int lvl;
 
 	public Monster(String name, int xp, int gold, int damage, int health, int lvlAvailable) {
-		if (!name.equalsIgnoreCase("Read Only")) {
-			this.name = name;
-			this.xp = xp;
-			this.gold = gold;
-			this.damage = damage;
-			this.health = health;
-			lvl = lvlAvailable;
-			AllMonsters.add(this);
-		}
+		this.name = name;
+		this.xp = xp;
+		this.gold = gold;
+		this.damage = damage;
+		this.health = health;
+		lvl = lvlAvailable;
+		AllMonsters.add(this);
+
 	}
-	public Monster pickMonster(Player a){
+
+	public static Monster pickMonster(Player a) {
 		Random rand = new Random();
-		Monster m = new Monster("Read Only", 0, 0, 0, 0, -1);
-		while(m.lvl > a.lvl){
-			//while required level is greater then player level, pick new monster
-			//this is so we pick a monster within the meetable monsters for this player
+		Monster m = AllMonsters.get(rand.nextInt(AllMonsters.size()));
+		while (m.lvl > a.lvl) {
+			// while required level is greater then player level, pick new
+			// monster
+			// this is so we pick a monster within the meetable monsters for
+			// this player
 			m = m.AllMonsters.get(rand.nextInt(m.AllMonsters.size()));
-			//this picks a random monster within AllMonsters
+			// this picks a random monster within AllMonsters
 		}
 		return m;
 	}
