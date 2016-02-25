849a32cca1be8a02f50701687041c28f6f20ceaf
Jake Michaletz <jakemichaletz01@gmail.com>
2016-02-25 14:06:40 -0700

    Jake's Amazing Change
    Fixed nap time. Caused interface to not stretch. :-)

--- src/main/Main.java
+++ src/main/Main.java
@@ -67,7 +67,7 @@ public class Main extends ConstructorClass {
 			@Override
 			public void onHover(){
 				System.out.println("Hovering over the Shop!");
-				h.nap(1000);
+				//h.nap(1000);
 			}
 		};
 		
