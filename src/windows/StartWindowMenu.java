d65a73e6e4229ea8e89d8d4c1176be95fa30ac53
MarkMousel <markmousel25@gmail.com>
2016-03-01 13:27:51 -0700

    My first commit

---
+++ src/windows/StartWindowMenu.java
@@ -0,0 +1,23 @@
+package windows;
+
+import java.awt.Color;
+
+import graphics.GraphicsImage;
+import main.Registry;
+
+public class StartWindowMenu extends WindowItem {
+	GraphicsImage startMenu = new GraphicsImage(Registry.imgRes.get("InventoryMenu"),200,0,200,50){
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
+
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		
+	}
