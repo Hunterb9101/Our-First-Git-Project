ea602d57d4ec371de063d8daa20ec65517e78f6d
toffles8 <codytoff@gmail.com>
2016-03-09 12:55:59 -0700

    Fightloop
    Fightloop

--- src/windows/FightLoopWindow.java
+++ src/windows/FightLoopWindow.java
@@ -1,13 +1,39 @@
 package windows;
 
 import java.awt.Graphics;
+import main.Main;
+import main.Monster;
+import main.Player;
+import main.Registry;
 
 public class FightLoopWindow extends WindowItem {
 	
-	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
 		
+		
+		Monster m = Monster.pickMonster(Main.me);
+		System.out.println(m.name);
+		System.out.println(Main.me.getAttackDamage());
+		System.out.println(m.damage);
+		System.out.println(m.health);
+		
+		if(Main.me.canAttack()){
+			m.health= m.health-Main.me.getAttackDamage();
+			System.out.println(m.health);
+		}
+		if(m.canAttack()){
+			Main.me.maxHp=Main.me.calculateMaxHP()-m.damage;
+			System.out.println(Main.me.maxHp);
+		}//if
+		
+		
+		
+		if(m.health<= 0){
+			System.out.println("Monster is dead you Win!");
+		}
+		else if(Main.me.maxHp<=0){
+			System.out.println("You died you lose!");
+		}
 	}
 
 }
