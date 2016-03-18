6d45c6b3dbedf7f2bb706c466a9605933a58d5c6
Hunterb9101 <hunterb9101@gmail.com>
2016-03-17 20:17:24 -0600

    Start of Region Package

---
+++ src/region/RegionAbstract.java
@@ -0,0 +1,9 @@
+package region;
+
+import java.util.ArrayList;
+
+public abstract class RegionAbstract {
+	public static ArrayList<RegionAbstract> allRegions = new ArrayList<RegionAbstract>();
+	public final int zoneNum = -1;
+	
+}
--- src/windows/MainWindow.java
+++ src/windows/MainWindow.java
@@ -4,26 +4,50 @@ import java.awt.Graphics;
 
 import graphics.GraphicsImage;
 import main.Registry;
+import region.RegionAbstract;
 
 public class MainWindow extends WindowItem {
-	private menuItem thisMenu = menuItem.NONE;
+	private menuItem thisMenu = menuItem.MAIN;
+	public int currZoneSelected = 0;
 	GraphicsImage mainMap = new GraphicsImage(Registry.loadImage("res/MainMap.png"),25,50,550,550, thisMenu);
-	GraphicsImage mapIcon = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),390,380,32,32, thisMenu);
-	GraphicsImage mapIcon2 = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),280,350,32,32, thisMenu);
+	
+	GraphicsImage mapIcon = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),390,380,32,32, thisMenu){
+		@Override
+		public void onClick(){
+			currZoneSelected = 1;
+		}
+	};
+	GraphicsImage mapIcon2 = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),280,350,32,32, thisMenu){
+		@Override
+		public void onClick(){
+			currZoneSelected = 2;
+		}
+	};
+	
 	GraphicsImage mapIcon3 = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),160,460,32,32, thisMenu);
 	GraphicsImage mapIcon4 = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),100,210,32,32, thisMenu);
 	GraphicsImage mapIcon5 = new GraphicsImage(Registry.loadImage("res/MapIcon.png"),270,180,32,32, thisMenu);
 	GraphicsImage mapIcon6 = new GraphicsImage(Registry.loadImage("res/mapIcon.png"),470,120,32,32, thisMenu);
+	
+	GraphicsImage zone1Bg = new GraphicsImage(Registry.loadImage("res/MtnBG.png"),0,0,600,600,thisMenu);
+	
 	@Override
 	public void draw() {
-		// TODO Auto-generated method stub
-		mainMap.drawObject();
-		mapIcon.drawObject();
-		mapIcon2.drawObject();
-		mapIcon3.drawObject();
-		mapIcon4.drawObject();
-		mapIcon5.drawObject();
-		mapIcon6.drawObject();
+		switch(currZoneSelected){
+		case 1:
+			zone1Bg.drawObject();
+			break;
+		case 2:
+			break;
+		default:
+			mainMap.drawObject();
+			mapIcon.drawObject();
+			mapIcon2.drawObject();
+			mapIcon3.drawObject();
+			mapIcon4.drawObject();
+			mapIcon5.drawObject();
+			mapIcon6.drawObject();
+		}
 	}
 
 }
